01328021f6687a475b3bebacc3ee4accb29cc0e7
Aditya Sahai <adityasahai@Adityas-MacBook-Air.local>
2026-06-19 15:59:52 +0530

    Add deployable three-world CSS overrides

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Cormorant, JetBrains_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import "./three-worlds.css";
 import GrainOverlay from "@/components/ui/GrainOverlay";
 import LenisProvider from "@/components/layout/LenisProvider";
 
